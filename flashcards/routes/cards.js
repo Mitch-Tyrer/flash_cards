f38ef2cf1d0a9dc20d59f68f249438e5613c49b6
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-22 06:13:52 -0700

    added random card generation

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -3,11 +3,33 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const randomNum = Math.floor(Math.random() * cards.length);
+    res.redirect(`/cards/${randomNum}`)
+});
+
 router.get('/:id', (req, res) => {
-    res.render('card', { 
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint
-    });
+    const {side} = req.query;
+    const {id} = req.params;
+    if(!side){
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const {hint} = cards[id];
+    const templateData = {id, text, name};
+
+    if (side === 'question'){
+        templateData.hint = hint;
+        templateData.sideToShow = "answer";
+        templateData.sideToShowDisplay = 'Answer';
+    } else if (side === 'answer'){
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question';
+    }
+
+
+    res.render('card', templateData);
 });
 
 module.exports = router;
